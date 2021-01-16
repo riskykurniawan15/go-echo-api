4be9c7326fbb7182d7beac0123825196f1ac8046
prashant-rathi <prashant.rathi@eagleview.com>
2021-01-16 12:39:28 +0530

    go-echo-api version updated

--- main.go
+++ main.go
@@ -24,5 +24,5 @@ func main() {
 
 }
 func Version(context echo.Context) error {
-	return context.JSON(http.StatusOK, map[string]interface{}{"version": 1})
+	return context.JSON(http.StatusOK, map[string]interface{}{"version": 2})
 }
